718ec924f51c91f32bb217008cec306902287e4b
P3ANTMC <pean.tmc@seznam.cz>
2024-02-16 20:32:25 +0100

    Ukazka razeni seznamu
    Zakladni razeni s vyuzitim
    'Comparator.comparting(.....)'
    
    Ukazka pouziti '.reversed()'

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 import cz.engeto.ja.Computer;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.math.BigDecimal;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         List<Computer> computers = new ArrayList<>();
 
-        Computer computer1 = new Computer("Dell", 2019, 15000);
-        Computer computer2 = new Computer("HP", 2018, 12000);
-        Computer computer3 = new Computer("Lenovo", 2017, 10000);
+        Computer computer1 = new Computer("Dell", 2019, BigDecimal.valueOf(15000));
+        Computer computer2 = new Computer("HP", 2018, BigDecimal.valueOf(12000));
+        Computer computer3 = new Computer("Lenovo", 2017, BigDecimal.valueOf(10000));
 
         computers.add(computer1);
         computers.add(computer2);
@@ -18,5 +18,12 @@ public class Main {
         for (Computer computer : computers) {
             System.out.println(computer.getDescription() + " " + computer.getYearOfProduction() + " " + computer.getPrice());
         }
+
+        computers.sort(Comparator.comparing(Computer::getPrice).reversed());
+
+        System.out.println("\nSorted by highest price:\n");
+        computers.forEach(System.out::println);
+
+
     }
 }
